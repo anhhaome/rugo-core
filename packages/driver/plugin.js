cc54fe896ef9d498a5f48e2876323a04fab2c528
Anh Hao <hi@anhhao.me>
2022-05-12 17:12:52 +0700

    Update plugin

---
+++ packages/driver/plugin.js
@@ -0,0 +1,29 @@
+import colors from 'colors';
+
+import { resolve } from 'path';
+import { BasePlugin } from 'rugo-common';
+import { createFsDriver, createMemDriver, createMongoDriver } from './src/index.js';
+
+export default {
+  ...BasePlugin,
+
+  name: 'driver',
+  async start(context) {
+    const { DRIVER_ROOT, DRIVER_URI } = process.env;
+    const root = DRIVER_ROOT ? resolve(DRIVER_ROOT) : null;
+
+    context.drivers = {
+      mem: root ? await createMemDriver({ root, cache: true }) : null,
+      fs: root ? await createFsDriver({ root, cache: true }) : null, 
+      mongo: DRIVER_URI ? await createMongoDriver({ uri: DRIVER_URI, cache: true }) : null, 
+    }
+
+    for (let key in context.drivers)
+      console.log(colors.yellow('[driver] ') + colors.white(`"${key}" driver is `) + (context.drivers[key] ? colors.green(`ready`) : colors.red(`not ready`)));
+  },
+  async close(context) {
+    for (let driverName in context.drivers)
+      if (context.drivers[driverName])
+        await context.drivers[driverName].close();
+  }
+}
