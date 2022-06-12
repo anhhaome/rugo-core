7d3b8c4e70531040a1762b91f50112161d02a24b
Vu Anh Hao <hi@anhhao.me>
2022-06-12 18:34:16 +0700

    Relation type

--- packages/model/src/types/index.js
+++ packages/model/src/types/index.js
@@ -1,5 +1,6 @@
 export * as boolean from './boolean.js';
+export * as file from './file.js';
 export * as number from './number.js';
 export * as password from './password.js';
+export * as relation from './relation.js';
 export * as text from './text.js';
-export * as file from './file.js';
